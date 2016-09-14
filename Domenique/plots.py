1ac2d24292184dbdaf9dc3a96eb15182c2800b53
Ammar Bahman <bahman.ammar@gmail.com>
2016-09-13 23:48:14 -0400

    update Dominique plots

--- Domenique/plots.py
+++ Domenique/plots.py
@@ -3,7 +3,7 @@ Created on Sep 13, 2016
 
 @author: ammarbahman
 
-Note: this plots for calorimter paper
+Note: this plots for VI compressor testing in calorimeter paper
 
 '''
 import numpy as np
@@ -50,8 +50,13 @@ mpl.rcParams.update(pgf_with_latex)
 # END of Latex render
 #===============================================================================
 
+#import the excel file
 df = pd.read_excel("results.xlsx")
 
+################################################################################
+# pressure ratio vs suction pressure
+################################################################################
+#assign axes
 x = np.array(df[1:]['PE1_p_comp_suc'], dtype=float) * 6.89476 #convert from psia to kPa
 y = np.array(df[1:]['PR'], dtype=float)
 c = np.array(df[1:]['PE2_p_comp_dis'], dtype=float) * 6.89476  # color of points (color bar points)
@@ -59,99 +64,224 @@ s = 60  # size of points
 
 fig, ax = plt.subplots()
 im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
-
 # Add a colorbar
 cbar = plt.colorbar(im, ax=ax)
-
 # set the color limits
 im.set_clim(1000, 2600)
 cbar.ax.set_ylabel('Discharge Pressure [kPa]')
-ax.text(0.75,0.9,'Markersize (speed) {:0.0f} Hz'.format(s),ha='center',va='center',transform = ax.transAxes)
+ax.text(0.8,0.95,'Markersize (speed) {:0.0f} Hz'.format(s),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
 #plt.ylim(0,22.5)
 #plt.xlim(0,24.875)
 plt.xlabel('Suction pressure [kPa]')
 plt.ylabel('Pressure ratio [-]')           
 plt.savefig('pressure_ratio.pdf')
-plt.show()
+#plt.show()
 plt.close()
 
-##################################################
 
+################################################################################
+# Norm suction mass vs Norm injection mass
+################################################################################
+#assign axes
 y = np.array(df[1:]['m_dot_ref']/df[1:]['m_dot_total'], dtype=float) * 100 #normalized flow rate
 x = np.array(df[1:]['m_dot_inj']/df[1:]['m_dot_total'], dtype=float) * 100 #normalized flow rate
 c = np.array(df[1:]['PR'], dtype=float)  # color of points (color bar points)
-s = 60  # size of points
+s = np.array(df[1:]['PE2_p_comp_dis'], dtype=float)  # size of points
 
 fig, ax = plt.subplots()
 im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
-
 # Add a colorbar
 cbar = plt.colorbar(im, ax=ax)
-
 # set the color limits
-im.set_clim(0, 6.4)
+im.set_clim(2, 7)
 cbar.ax.set_ylabel('Pressure ratio [-]')
-ax.text(0.75,0.9,'Markersize (speed) {:0.0f} Hz'.format(s),ha='center',va='center',transform = ax.transAxes)
+ax.text(0.75,0.95,'Markersize ($P_{dis}$)'+' {:0.0f} to '.format(np.min(s)) +'{:0.0f} psia'.format(np.max(s)),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
 plt.ylim(70,100)
 plt.xlim(0,30)
 plt.ylabel('Norm. suction mass flow rate [\%]')
-plt.xlabel('Norm. injection mass flow rate [\%]')
-              
+plt.xlabel('Norm. injection mass flow rate [\%]')            
 plt.savefig('injection_flow.pdf')
-plt.show()
+#plt.show()
 plt.close()
 
-##################################################
 
-
+################################################################################
+# suction superheat vs suction pressure
+################################################################################
+#assign axes
 x = np.array(df[1:]['PE1_p_comp_suc'], dtype=float) * 6.89476 #convert from psia to kPa
 y = np.array(df[1:]['DELTAT_sh_suc'], dtype=float) * 0.555556 #convert delta R to delta K
 c = np.array(df[1:]['PR'], dtype=float)  # color of points (color bar points)
-s = 60  # size of points
+s = np.array(df[1:]['PE2_p_comp_dis'], dtype=float)  # size of points
 
 fig, ax = plt.subplots()
 im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
-
 # Add a colorbar
 cbar = plt.colorbar(im, ax=ax)
-
 # set the color limits
-im.set_clim(0, 6.4)
+im.set_clim(2, 7)
 cbar.ax.set_ylabel('Pressure ratio[-]')
-ax.text(0.75,0.9,'Markersize (speed) {:0.0f} Hz'.format(s),ha='center',va='center',transform = ax.transAxes)
-#plt.ylim(0,22.5)
+ax.text(0.75,0.95,'Markersize ($P_{dis}$)'+' {:0.0f} to '.format(np.min(s)) +'{:0.0f} psia'.format(np.max(s)),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
+plt.ylim(0,20)
 #plt.xlim(0,24.875)
 plt.xlabel('Suction pressure [kPa]')
 plt.ylabel('Suction superheat [K]')           
 plt.savefig('suction_superheat.pdf')
-plt.show()
+#plt.show()
 plt.close()
 
 
-##################################################
-
-
+################################################################################
+# injection superheat vs suction superheat
+################################################################################
+#assign axes
 x = np.array(df[1:]['DELTAT_sh_suc'], dtype=float) * 0.555556 #convert delta R to delta K
 y = np.array(df[1:]['DELTAT_sh_inj'], dtype=float) * 0.555556 #convert delta R to delta K
 c = np.array(df[1:]['PR'], dtype=float)  # color of points (color bar points)
+s = np.array(df[1:]['PE2_p_comp_dis'], dtype=float)  # size of points
+
+fig, ax = plt.subplots()
+im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
+# Add a colorbar
+cbar = plt.colorbar(im, ax=ax)
+# set the color limits
+im.set_clim(2, 7)
+cbar.ax.set_ylabel('Pressure ratio[-]')
+ax.text(0.75,0.95,'Markersize ($P_{dis}$)'+' {:0.0f} to '.format(np.min(s)) +'{:0.0f} psia'.format(np.max(s)),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
+plt.ylim(0,40)
+plt.xlim(0,20)
+plt.xlabel('Suction superheat [K]')
+plt.ylabel('Injection superheat [K]')           
+plt.savefig('injection_superheat.pdf')
+#plt.show()
+plt.close()
+
+
+################################################################################
+# heat loss vs discharge temp
+################################################################################
+#assign axes
+x = (np.array(df[1:]['TC3_T_comp_dis'], dtype=float) + 459.67) * 5.0/9.0 #convert F to  K
+y = np.array(df[1:]['f_Q'], dtype=float)
+c = (np.array(df[1:]['T_inj_dewpt'], dtype=float) + 459.67) * 5.0/9.0 # color of points (color bar points)
+s = np.array(df[1:]['PE2_p_comp_dis'], dtype=float)  # size of points
+
+fig, ax = plt.subplots()
+im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
+# Add a colorbar
+cbar = plt.colorbar(im, ax=ax)
+# set the color limits
+#im.set_clim(2, 7)
+cbar.ax.set_ylabel('Saturated injection temperature [K]')
+ax.text(0.75,0.95,'Markersize ($P_{dis}$)'+' {:0.0f} to '.format(np.min(s)) +'{:0.0f} psia'.format(np.max(s)),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
+#plt.ylim(0,40)
+#plt.xlim(0,20)
+plt.xlabel('Discharge temperature [K]')
+plt.ylabel('Heat loss in \% of input power [-]')           
+plt.savefig('heat_loss.pdf')
+#plt.show()
+plt.close()
+
+
+################################################################################
+# isentropic eff vs volume ratio
+################################################################################
+#assign axes
+x = np.array(df[1:]['VolumeRatio'], dtype=float) 
+y = np.array(df[1:]['eta_isen'], dtype=float)
+c = np.array(df[1:]['PR'], dtype=float) # color of points (color bar points)
 s = 60  # size of points
 
 fig, ax = plt.subplots()
 im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
-
 # Add a colorbar
 cbar = plt.colorbar(im, ax=ax)
-
 # set the color limits
-im.set_clim(0, 6.4)
-cbar.ax.set_ylabel('Pressure ratio[-]')
-ax.text(0.75,0.9,'Markersize (speed) {:0.0f} Hz'.format(s),ha='center',va='center',transform = ax.transAxes)
-#plt.ylim(0,22.5)
-#plt.xlim(0,24.875)
-plt.xlabel('Suction superheat [K]')
-plt.ylabel('Injection superheat [K]')           
-plt.savefig('injection_superheat.pdf')
-plt.show()
+im.set_clim(2, 7)
+cbar.ax.set_ylabel('Pressure ratio [-]')
+ax.text(0.8,0.95,'Markersize (speed) {:0.0f} Hz'.format(s),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
+plt.ylim(60,70)
+plt.xlim(2,6)
+plt.xlabel('Volume ratio ($v_{suc}/v_{dis}$) [-]')
+plt.ylabel('Overall isentropic efficiency [\%]')           
+plt.savefig('isentropic_eff.pdf')
+#plt.show()
 plt.close()
 
 
+################################################################################
+# heat loss vs ambient temp
+################################################################################
+#assign axes
+x = (np.array(df[1:]['TC12_T_chamb_amb'], dtype=float) + 459.67) * 5.0/9.0 #convert F to K
+y = np.array(df[1:]['Q_dot_loss'], dtype=float) * 0.29307107 # convert btu/hr to Watts
+c = (np.array(df[1:]['T_inj_dewpt'], dtype=float) + 459.67) * 5.0/9.0 # color of points (color bar points)
+s = 60  # size of points
+
+fig, ax = plt.subplots()
+im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
+# Add a colorbar
+cbar = plt.colorbar(im, ax=ax)
+# set the color limits
+#im.set_clim(2, 7)
+cbar.ax.set_ylabel('Saturated injection temperature [K]')
+ax.text(0.8,0.95,'Markersize (speed) {:0.0f} Hz'.format(s),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
+#plt.ylim(60,70)
+plt.xlim(300,316)
+plt.xlabel('Ambient temperature [K]')
+plt.ylabel('Heat loss [W]')           
+plt.savefig('heat_loss_ambient.pdf')
+#plt.show()
+plt.close()
+
+
+################################################################################
+# discharge volume ratio eff vs pressure ratio
+################################################################################
+#assign axes
+x = np.array(df[1:]['PR'], dtype=float)
+y = np.array(df[1:]['DischargeVolumeRatio'], dtype=float)
+c = np.array(df[1:]['NormalizedInjectionMassFlow'], dtype=float) * 100 # color of points (color bar points)
+s = np.array(df[1:]['PE2_p_comp_dis'], dtype=float)  # size of points
+
+fig, ax = plt.subplots()
+im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
+# Add a colorbar
+cbar = plt.colorbar(im, ax=ax)
+# set the color limits
+im.set_clim(0, 30)
+cbar.ax.set_ylabel('Normalized injection mass flow rate [\%]')
+ax.text(0.75,0.95,'Markersize ($P_{dis}$)'+' {:0.0f} to '.format(np.min(s)) +'{:0.0f} psia'.format(np.max(s)),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
+#plt.ylim(60,70)
+#plt.xlim(300,316)
+plt.xlabel('Pressure ratio [-]')
+plt.ylabel('Discharge pocket volume ratio [-]')           
+plt.savefig('discharge_volume_ratio.pdf')
+#plt.show()
+plt.close()
+
+
+################################################################################
+# isentropic eff vs pressure ratio
+################################################################################
+#assign axes
+x = np.array(df[1:]['PR'], dtype=float) 
+y = np.array(df[1:]['eta_isen'], dtype=float)
+c = np.array(df[1:]['NormalizedInjectionMassFlow'], dtype=float) * 100 # color of points (color bar points)
+s = np.array(df[1:]['PE2_p_comp_dis'], dtype=float)  # size of points
+
+fig, ax = plt.subplots()
+im = ax.scatter(x, y, c=c, s=s, cmap=plt.cm.jet)
+# Add a colorbar
+cbar = plt.colorbar(im, ax=ax)
+# set the color limits
+im.set_clim(0, 30)
+cbar.ax.set_ylabel('Normalized injection mass flow rate [\%]')
+ax.text(0.75,0.95,'Markersize ($P_{dis}$)'+' {:0.0f} to '.format(np.min(s)) +'{:0.0f} psia'.format(np.max(s)),ha='center',va='center',transform = ax.transAxes,fontsize = 8)
+plt.ylim(60,70)
+plt.xlim(2,7)
+plt.xlabel('Pressure ratio [-]')
+plt.ylabel('Overall isentropic efficiency [\%]')           
+plt.savefig('isentropic_eff_PR.pdf')
+plt.show()
+plt.close()
