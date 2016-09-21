0a44fb2649bba4888dac78f7c061f7be7cb3bf0a
Ammar Bahman <bahman.ammar@gmail.com>
2016-09-20 21:24:56 -0400

    add wider image for the IJR paper

--- Interleaved-Christian/evap_N_circ_cross_couter_flow/Tuning/Evaporatorparity_baseline_3Js.py
+++ Interleaved-Christian/evap_N_circ_cross_couter_flow/Tuning/Evaporatorparity_baseline_3Js.py
@@ -10,6 +10,7 @@ import shutil
 from scipy import polyval, polyfit
 import pandas as pd
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 def figsize(scale):
     fig_width_pt = 469.755                          # Get this from LaTeX using \the\textwidth
@@ -81,72 +82,73 @@ mape_Q = mape(Q_model,Q_exp)
 print("mape_mass error is: " + str(mape_mass) + " %")
 print("mape_Q error is: " + str(mape_Q) + " %")
 
-f=pylab.figure(figsize=(3.5,3.5))
-ax=f.add_axes((0.18,0.15,0.77,0.8))
-
-w=rmse_mass #Error
-ax_max = 0.2 #x and y-axes max scale tick
-upp_txt = ax_max / 2.2 #location of upper error text on plot -- adjust the number to adjust the location
-low_txt = ax_max / 2.0 #location of lower error text on plot -- adjust the number to adjust the location
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max],'k-',lw=1)
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w)],'k-.',lw=1)
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w)],'k-.',lw=1)
-ax.text(low_txt-0.002,low_txt*(1-w),'-{:0.0f}\%'.format(w*100),ha='left',va='top')
-ax.text(upp_txt-0.002,upp_txt*(1+w),'+{:0.0f}\%'.format(w*100),ha='right',va='bottom')
-##########
-#comment out if second error line is not required
-# w1=0.10 #Error
-# upp_txt = ax_max / 1.3 #location of upper error text on plot -- adjust the number to adjust the location
-# low_txt = ax_max / 1.15 #location of lower error text on plot -- adjust the number to adjust the location
-# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w1)],'k--',lw=1)
-# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w1)],'k--',lw=1)
-# ax.text(low_txt-0.002,low_txt*(1-w1),'-%0.0f%%' %(w1*100),ha='left',va='top')
-# ax.text(upp_txt-0.002,upp_txt*(1+w1),'+%0.0f%%' %(w1*100),ha='right',va='bottom')
-#########
-ax.set_xlabel('$\dot m_{exp}$ [kg/s]')
-ax.set_ylabel('$\dot m_{model}$ [kg/s]')
-ax.plot(mdot_exp,mdot_model,'o',ms=4,markerfacecolor='None',label='Mass flow rate',mec='b',mew=1)
-leg=ax.legend(loc='upper left',numpoints=1)
-frame  = leg.get_frame()  
-frame.set_linewidth(0.5)
-ax.set_xlim((0,ax_max))
-ax.set_ylim((0,ax_max))
-pylab.savefig('images_baseline/Evap_parity_baseline_mass_3Js.pdf')
-pylab.show()
-pylab.close()
-
-
-#####cooling capacity plot#####
-f=pylab.figure(figsize=(3.5,3.5))
-ax=f.add_axes((0.18,0.15,0.77,0.8))
-
-w=rmse_Q #Error
-ax_max = 30 #x and y-axes max scale tick
-upp_txt = ax_max / 1.8 #location of upper error text on plot -- adjust the number to adjust the location
-low_txt = ax_max / 1.3 #location of lower error text on plot -- adjust the number to adjust the location
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max],'k-',lw=1)
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w)],'k-.',lw=1)
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w)],'k-.',lw=1)
-ax.text(low_txt-0.002,low_txt*(1-w),'-{:0.0f}\%'.format(w*100),ha='left',va='top')
-ax.text(upp_txt-0.002,upp_txt*(1+w),'+{:0.0f}\%'.format(w*100),ha='right',va='bottom')
-ax.set_xlabel('$\dot Q_{exp}$ [kW]')
-ax.set_ylabel('$\dot Q_{model}$ [kW]')
-ax.plot(Q_exp,Q_model,'s',ms=4,markerfacecolor='None',label='Cooling capacity',mec='r',mew=1)
-leg=ax.legend(loc='upper left',numpoints=1)
-frame  = leg.get_frame()  
-frame.set_linewidth(0.5)
-ax.set_xlim((0,ax_max))
-ax.set_ylim((0,ax_max))
-pylab.savefig('images_baseline/Evap_parity_baseline_Q_3Js.pdf')
-pylab.show()
-pylab.close()
+# f=pylab.figure(figsize=(3.5,3.5))
+# ax=f.add_axes((0.18,0.15,0.77,0.8))
+# 
+# w=rmse_mass #Error
+# ax_max = 0.2 #x and y-axes max scale tick
+# upp_txt = ax_max / 2.2 #location of upper error text on plot -- adjust the number to adjust the location
+# low_txt = ax_max / 2.0 #location of lower error text on plot -- adjust the number to adjust the location
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max],'k-',lw=1)
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w)],'k-.',lw=1)
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w)],'k-.',lw=1)
+# ax.text(low_txt-0.002,low_txt*(1-w),'-{:0.0f}\%'.format(w*100),ha='left',va='top')
+# ax.text(upp_txt-0.002,upp_txt*(1+w),'+{:0.0f}\%'.format(w*100),ha='right',va='bottom')
+# ##########
+# #comment out if second error line is not required
+# # w1=0.10 #Error
+# # upp_txt = ax_max / 1.3 #location of upper error text on plot -- adjust the number to adjust the location
+# # low_txt = ax_max / 1.15 #location of lower error text on plot -- adjust the number to adjust the location
+# # ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w1)],'k--',lw=1)
+# # ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w1)],'k--',lw=1)
+# # ax.text(low_txt-0.002,low_txt*(1-w1),'-%0.0f%%' %(w1*100),ha='left',va='top')
+# # ax.text(upp_txt-0.002,upp_txt*(1+w1),'+%0.0f%%' %(w1*100),ha='right',va='bottom')
+# #########
+# ax.set_xlabel('$\dot m_{exp}$ [kg/s]')
+# ax.set_ylabel('$\dot m_{model}$ [kg/s]')
+# ax.plot(mdot_exp,mdot_model,'o',ms=4,markerfacecolor='None',label='Mass flow rate',mec='b',mew=1)
+# leg=ax.legend(loc='upper left',numpoints=1)
+# frame  = leg.get_frame()  
+# frame.set_linewidth(0.5)
+# ax.set_xlim((0,ax_max))
+# ax.set_ylim((0,ax_max))
+# pylab.savefig('images_baseline/Evap_parity_baseline_mass_3Js.pdf')
+# pylab.show()
+# pylab.close()
+# 
+# 
+# #####cooling capacity plot#####
+# f=pylab.figure(figsize=(3.5,3.5))
+# ax=f.add_axes((0.18,0.15,0.77,0.8))
+# 
+# w=rmse_Q #Error
+# ax_max = 30 #x and y-axes max scale tick
+# upp_txt = ax_max / 1.8 #location of upper error text on plot -- adjust the number to adjust the location
+# low_txt = ax_max / 1.3 #location of lower error text on plot -- adjust the number to adjust the location
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max],'k-',lw=1)
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w)],'k-.',lw=1)
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w)],'k-.',lw=1)
+# ax.text(low_txt-0.002,low_txt*(1-w),'-{:0.0f}\%'.format(w*100),ha='left',va='top')
+# ax.text(upp_txt-0.002,upp_txt*(1+w),'+{:0.0f}\%'.format(w*100),ha='right',va='bottom')
+# ax.set_xlabel('$\dot Q_{exp}$ [kW]')
+# ax.set_ylabel('$\dot Q_{model}$ [kW]')
+# ax.plot(Q_exp,Q_model,'s',ms=4,markerfacecolor='None',label='Cooling capacity',mec='r',mew=1)
+# leg=ax.legend(loc='upper left',numpoints=1)
+# frame  = leg.get_frame()  
+# frame.set_linewidth(0.5)
+# ax.set_xlim((0,ax_max))
+# ax.set_ylim((0,ax_max))
+# pylab.savefig('images_baseline/Evap_parity_baseline_Q_3Js.pdf')
+# pylab.show()
+# pylab.close()
 
 
 #########################
 #####Combined plot#######
 #########################
-f=pylab.figure(figsize=(3.5,3.5))
-ax=f.add_axes((0.18,0.15,0.77,0.8))
+#f=pylab.figure(figsize=(3.5,3.5))
+#ax=f.add_axes((0.18,0.15,0.77,0.8))
+fig, ax = plt.subplots()
 
 w=0.1357 #Error
 ax_max = 2 #x and y-axes max scale tick
@@ -166,7 +168,7 @@ frame  = leg.get_frame()
 frame.set_linewidth(0.5)
 ax.set_xlim((0,ax_max))
 ax.set_ylim((0,ax_max))
-pylab.savefig('images_baseline/Evap_parity_baseline_combined_3Js.pdf')
+pylab.savefig('images_baseline/Evap_parity_baseline_combined_3Js_wider.pdf')
 pylab.show()
 pylab.close()
 
@@ -198,71 +200,72 @@ mape_Q = mape(Q_model,Q_exp)
 print("mape_mass error is: " + str(mape_mass)+" %")
 print("mape_Q error is: " + str(mape_Q) +" %")
 
-f=pylab.figure(figsize=(3.5,3.5))
-ax=f.add_axes((0.18,0.15,0.77,0.8))
-
-w=rmse_mass #Error
-ax_max = 0.2 #x and y-axes max scale tick
-upp_txt = ax_max / 2.2 #location of upper error text on plot -- adjust the number to adjust the location
-low_txt = ax_max / 2.0 #location of lower error text on plot -- adjust the number to adjust the location
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max],'k-',lw=1)
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w)],'k-.',lw=1)
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w)],'k-.',lw=1)
-ax.text(low_txt-0.002,low_txt*(1-w),'-{:0.0f}\%'.format(w*100),ha='left',va='top')
-ax.text(upp_txt-0.002,upp_txt*(1+w),'+{:0.0f}\%'.format(w*100),ha='right',va='bottom')
-##########
-#comment out if second error line is not required
-# w1=0.10 #Error
-# upp_txt = ax_max / 1.3 #location of upper error text on plot -- adjust the number to adjust the location
-# low_txt = ax_max / 1.15 #location of lower error text on plot -- adjust the number to adjust the location
-# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w1)],'k--',lw=1)
-# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w1)],'k--',lw=1)
-# ax.text(low_txt-0.002,low_txt*(1-w1),'-%0.0f%%' %(w1*100),ha='left',va='top')
-# ax.text(upp_txt-0.002,upp_txt*(1+w1),'+%0.0f%%' %(w1*100),ha='right',va='bottom')
-#########
-ax.set_xlabel('$\dot m_{exp}$ [kg/s]')
-ax.set_ylabel('$\dot m_{model}$ [kg/s]')
-ax.plot(mdot_exp,mdot_model,'o',ms=4,markerfacecolor='None',label='Mass flow rate',mec='b',mew=1)
-leg=ax.legend(loc='upper left',numpoints=1)
-frame  = leg.get_frame()  
-frame.set_linewidth(0.5)
-ax.set_xlim((0,ax_max))
-ax.set_ylim((0,ax_max))
-pylab.savefig('images_baseline/Evap_parity_baseline_mass_PedictInterleaved_3Js.pdf')
-pylab.show()
-pylab.close()
-
-
-#####cooling capacity plot#####
-f=pylab.figure(figsize=(3.5,3.5))
-ax=f.add_axes((0.18,0.15,0.77,0.8))
-
-w=rmse_Q #Error
-ax_max = 30 #x and y-axes max scale tick
-upp_txt = ax_max / 1.8 #location of upper error text on plot -- adjust the number to adjust the location
-low_txt = ax_max / 1.3 #location of lower error text on plot -- adjust the number to adjust the location
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max],'k-',lw=1)
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w)],'k-.',lw=1)
-ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w)],'k-.',lw=1)
-ax.text(low_txt-0.002,low_txt*(1-w),'-{:0.0f}\%'.format(w*100),ha='left',va='top')
-ax.text(upp_txt-0.002,upp_txt*(1+w),'+{:0.0f}\%'.format(w*100),ha='right',va='bottom')
-ax.set_xlabel('$\dot Q_{exp}$ [kW]')
-ax.set_ylabel('$\dot Q_{model}$ [kW]')
-ax.plot(Q_exp,Q_model,'s',ms=4,markerfacecolor='None',label='Cooling capacity',mec='r',mew=1)
-leg=ax.legend(loc='upper left',numpoints=1)
-frame  = leg.get_frame()  
-frame.set_linewidth(0.5)
-ax.set_xlim((0,ax_max))
-ax.set_ylim((0,ax_max))
-pylab.savefig('images_baseline/Evap_parity_baseline_Q_PredictInterleaved_3Js.pdf')
-pylab.show()
-pylab.close()
+# f=pylab.figure(figsize=(3.5,3.5))
+# ax=f.add_axes((0.18,0.15,0.77,0.8))
+# 
+# w=rmse_mass #Error
+# ax_max = 0.2 #x and y-axes max scale tick
+# upp_txt = ax_max / 2.2 #location of upper error text on plot -- adjust the number to adjust the location
+# low_txt = ax_max / 2.0 #location of lower error text on plot -- adjust the number to adjust the location
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max],'k-',lw=1)
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w)],'k-.',lw=1)
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w)],'k-.',lw=1)
+# ax.text(low_txt-0.002,low_txt*(1-w),'-{:0.0f}\%'.format(w*100),ha='left',va='top')
+# ax.text(upp_txt-0.002,upp_txt*(1+w),'+{:0.0f}\%'.format(w*100),ha='right',va='bottom')
+# ##########
+# #comment out if second error line is not required
+# # w1=0.10 #Error
+# # upp_txt = ax_max / 1.3 #location of upper error text on plot -- adjust the number to adjust the location
+# # low_txt = ax_max / 1.15 #location of lower error text on plot -- adjust the number to adjust the location
+# # ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w1)],'k--',lw=1)
+# # ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w1)],'k--',lw=1)
+# # ax.text(low_txt-0.002,low_txt*(1-w1),'-%0.0f%%' %(w1*100),ha='left',va='top')
+# # ax.text(upp_txt-0.002,upp_txt*(1+w1),'+%0.0f%%' %(w1*100),ha='right',va='bottom')
+# #########
+# ax.set_xlabel('$\dot m_{exp}$ [kg/s]')
+# ax.set_ylabel('$\dot m_{model}$ [kg/s]')
+# ax.plot(mdot_exp,mdot_model,'o',ms=4,markerfacecolor='None',label='Mass flow rate',mec='b',mew=1)
+# leg=ax.legend(loc='upper left',numpoints=1)
+# frame  = leg.get_frame()  
+# frame.set_linewidth(0.5)
+# ax.set_xlim((0,ax_max))
+# ax.set_ylim((0,ax_max))
+# pylab.savefig('images_baseline/Evap_parity_baseline_mass_PedictInterleaved_3Js.pdf')
+# pylab.show()
+# pylab.close()
+# 
+# 
+# #####cooling capacity plot#####
+# f=pylab.figure(figsize=(3.5,3.5))
+# ax=f.add_axes((0.18,0.15,0.77,0.8))
+# 
+# w=rmse_Q #Error
+# ax_max = 30 #x and y-axes max scale tick
+# upp_txt = ax_max / 1.8 #location of upper error text on plot -- adjust the number to adjust the location
+# low_txt = ax_max / 1.3 #location of lower error text on plot -- adjust the number to adjust the location
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max],'k-',lw=1)
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1-w)],'k-.',lw=1)
+# ax.plot(np.r_[0,ax_max],np.r_[0,ax_max*(1+w)],'k-.',lw=1)
+# ax.text(low_txt-0.002,low_txt*(1-w),'-{:0.0f}\%'.format(w*100),ha='left',va='top')
+# ax.text(upp_txt-0.002,upp_txt*(1+w),'+{:0.0f}\%'.format(w*100),ha='right',va='bottom')
+# ax.set_xlabel('$\dot Q_{exp}$ [kW]')
+# ax.set_ylabel('$\dot Q_{model}$ [kW]')
+# ax.plot(Q_exp,Q_model,'s',ms=4,markerfacecolor='None',label='Cooling capacity',mec='r',mew=1)
+# leg=ax.legend(loc='upper left',numpoints=1)
+# frame  = leg.get_frame()  
+# frame.set_linewidth(0.5)
+# ax.set_xlim((0,ax_max))
+# ax.set_ylim((0,ax_max))
+# pylab.savefig('images_baseline/Evap_parity_baseline_Q_PredictInterleaved_3Js.pdf')
+# pylab.show()
+# pylab.close()
 
 #########################
 #####Combined plot#######
 #########################
-f=pylab.figure(figsize=(3.5,3.5))
-ax=f.add_axes((0.18,0.15,0.77,0.8))
+# f=pylab.figure(figsize=(3.5,3.5))
+# ax=f.add_axes((0.18,0.15,0.77,0.8))
+fig, ax = plt.subplots()
 
 w=0.1241 #Error
 ax_max = 2 #x and y-axes max scale tick
@@ -282,6 +285,6 @@ frame  = leg.get_frame()
 frame.set_linewidth(0.5)
 ax.set_xlim((0,ax_max))
 ax.set_ylim((0,ax_max))
-pylab.savefig('images_baseline/Evap_parity_baseline_combined_PredictInterleaved_3Js.pdf')
+pylab.savefig('images_baseline/Evap_parity_baseline_combined_PredictInterleaved_3Js_wider.pdf')
 pylab.show()
 pylab.close()
