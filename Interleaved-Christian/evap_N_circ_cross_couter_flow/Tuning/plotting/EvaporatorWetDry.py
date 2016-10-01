d3acac8d61569fb53b0a0929644f8e75432cbb6a
Ammar Bahman <bahman.ammar@gmail.com>
2016-09-30 23:14:41 -0400

    new evaporator dry wet section plot

--- Interleaved-Christian/evap_N_circ_cross_couter_flow/Tuning/plotting/EvaporatorWetDry.py
+++ Interleaved-Christian/evap_N_circ_cross_couter_flow/Tuning/plotting/EvaporatorWetDry.py
@@ -42,48 +42,70 @@ mpl.rcParams.update(pgf_with_latex)
 # END of Latex render
 #===============================================================================
 
-fig=pylab.figure(figsize=(8,4))
+#fig=pylab.figure(figsize=(8,4))
+fig = pylab.figure()
+ax = fig.add_subplot(111)
 
+#draw upper/lower lines + blue fill
 pylab.fill(np.r_[0,5,5,0,0],np.r_[-1,-1,0,0,-1],'b',alpha=0.1)
 pylab.plot(np.r_[0,5],np.r_[1,1],'k')
 pylab.plot(np.r_[0,5],np.r_[0,0],'k')
 pylab.plot(np.r_[0,5],np.r_[-1,-1],'k')
 
-pylab.plot(np.r_[3,3],np.r_[-1,1],'k--')
-
-pylab.plot(3,0.5,'ko')
-pylab.plot(3,-0.5,'ko')
-pylab.text(3.05,0.5,'$T_{a,x}$',ha='left',va='center')
-pylab.text(3.05,-0.5,'$T_{g,x}$',ha='left',va='center')
-
-pylab.plot(0,0.5,'ko')
-pylab.plot(0,-0.5,'ko')
-pylab.text(0.05,0.5,'$T_{a,i}$',ha='left',va='center')
-pylab.text(0.05,-0.5,'$T_{g,o}$',ha='left',va='center')
-
-pylab.plot(5,0.5,'ko')
-pylab.plot(5,-0.5,'ko')
-pylab.text(5.05,0.5,'$T_{a,o}$',ha='left',va='center')
-pylab.text(5.05,-0.5,'$T_{g,i}$',ha='left',va='center')
-
-pylab.plot(5,0.5,'ko')
-pylab.plot(5,-0.5,'ko')
-pylab.text(5.05,0.5,'$T_{a,o}$',ha='left',va='center')
-pylab.text(5.05,-0.5,'$T_{g,i}$',ha='left',va='center')
-pylab.gca().add_patch(FancyArrowPatch((1,0.5),(0.5,0.5),arrowstyle='<|-',fc='k',ec='k',mutation_scale=20,lw=0.8))
+#text
+pylab.text(4,-1.1,'Wet section',ha='center',va='top')
+pylab.text(1.5,-1.1,'Dry section',ha='center',va='top')
 
 #Wet line
-pylab.plot(np.r_[3,5],np.r_[0.01,0.01],'b',lw=4)
+# pylab.plot(np.r_[3,5],np.r_[0.01,0.01],'b',lw=4)
 
-pylab.plot(0,0,'ro')
-pylab.text(-0.05,0,'$T_{i,s}$',ha='right',va='center')
+#seperation line
+pylab.plot(np.r_[3,3],np.r_[-1.325,1.325],'k--')
 
-pylab.plot(5,0,'ro')
-pylab.text(5.05,0,'$T_{o,s}$',ha='left',va='center')
+#draw dots and Temperature symbol
+pylab.plot(3,0,'ko')
+#pylab.plot(3,-0.5,'ko')
+# pylab.text(3.05,0.5,'$T_{a,x}$',ha='left',va='center')
+# pylab.text(3.05,-0.5,'$T_{g,x}$',ha='left',va='center')
 
-pylab.text(4,-1.1,'Wet Part',ha='center',va='top')
-pylab.text(1.5,-1.1,'Dry Part',ha='center',va='top')
-pylab.gca().add_patch(FancyArrowPatch((4,-0.5),(4.5,-0.5),arrowstyle='<|-',fc='k',ec='k',mutation_scale=20,lw=0.8))
+# pylab.plot(0,0.5,'ko')
+# pylab.plot(0,-0.5,'ko')
+# pylab.text(0.05,0.5,'$T_{a,i}$',ha='left',va='center')
+# pylab.text(0.05,-0.5,'$T_{g,o}$',ha='left',va='center')
+#
+# pylab.plot(5,0.5,'ko')
+# pylab.plot(5,-0.5,'ko')
+# pylab.text(5.05,0.5,'$T_{a,o}$',ha='left',va='center')
+# pylab.text(5.05,-0.5,'$T_{g,i}$',ha='left',va='center')
+
+# pylab.plot(5,0.5,'ko')
+# pylab.plot(5,-0.5,'ko')
+# pylab.text(5.05,0.5,'$T_{a,o}$',ha='left',va='center')
+# pylab.text(5.05,-0.5,'$T_{g,i}$',ha='left',va='center')
+pylab.text(1.5,1.5,'Wall temperature at\n dew-point of air',ha='center',va='bottom')
+pylab.gca().add_patch(FancyArrowPatch((3,0),(1.5,1.5),arrowstyle='<|-',fc='k',ec='k',mutation_scale=20,lw=0.8))
+#pylab.gca().add_patch(FancyArrowPatch((4,-0.5),(4.5,-0.5),arrowstyle='<|-',fc='k',ec='k',mutation_scale=20,lw=0.8))
+
+# pylab.plot(0,0,'ro')
+# pylab.text(-0.05,0,'$T_{i,s}$',ha='right',va='center')
+# 
+# pylab.plot(5,0,'ro')
+# pylab.text(5.05,0,'$T_{o,s}$',ha='left',va='center')
+
+#plot arrows
+bbox_props = dict(boxstyle="rarrow", fc="w", ec="k", lw=1)
+t1 = ax.text(0, 0.5, "Air flow", color="k", ha="center", va="center", rotation=0,
+            size=10,
+            bbox=bbox_props)
+bb = t1.get_bbox_patch()
+bb.set_boxstyle("rarrow", pad=0.5)
+
+t1 = ax.text(4.75, -0.5, "Refrigerant flow", color="k", ha="center", va="center", rotation=0,
+            size=10,
+            bbox=bbox_props)
+bb = t1.get_bbox_patch()
+bb.set_boxstyle("larrow", pad=0.5)
+
 
 pylab.gca().set_xlim(-0.1,5.1)
 pylab.gca().axis('equal')
