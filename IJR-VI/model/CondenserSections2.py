c4dfe74b46e7d7e9e1a12aa04ecf8aafc9e799b9
Ammar Bahman <bahman.ammar@gmail.com>
2018-03-09 22:39:37 -0500

    update figure

--- IJR-VI/model/CondenserSections2.py
+++ IJR-VI/model/CondenserSections2.py
@@ -43,7 +43,8 @@ mpl.rcParams.update(pgf_with_latex)
 #===============================================================================
 
 fig=pylab.figure()
-pylab.plot(np.r_[-0.01,4.01,4.01,-0.01,-0.01],np.r_[0,0,1,1,0],'k')
+pylab.plot(np.r_[0.0,4.0,4.0,0.0,0.0],np.r_[0,0,1,1,0],'k')
+pylab.plot(np.r_[0,4],np.r_[0.993,0.993],'k')
 
 pylab.plot(np.r_[1,1],np.r_[0,1],'k-.')
 pylab.plot(np.r_[3,3],np.r_[0,1],'k-.')
@@ -60,11 +61,11 @@ pylab.gca().text(3.5,-0.1,'$w_{superheat}$',ha='center',va='center')
 #plot color gradient 1st plot
 gradient = np.linspace(0, 1, 2)
 gradient = np.vstack((gradient, gradient))
-img = pylab.imshow(gradient, extent=[0,4.01,0,0.999], alpha=0.9, aspect=4, cmap=plt.get_cmap('brg'))
+img = pylab.imshow(gradient, extent=[0,4.0,0,1.0], alpha=0.9, aspect=4, cmap=plt.get_cmap('brg'))
 img.set_clim(0.1,2.0)
 
 y0=1.7 #shift of second axis
-pylab.plot(np.r_[-0.01,4.01,4.01,-0.01,-0.01],np.r_[-y0,-y0,-y0+1,-y0+1,-y0],'k')
+pylab.plot(np.r_[0.0,4.0,4.0,0.0,0.0],np.r_[-y0,-y0,-y0+1,-y0+1,-y0],'k')
 pylab.plot(np.r_[3,3],np.r_[-y0,-y0+1],'k-.')
 pylab.gca().text(1.5,-y0+0.5,'Two-Phase',ha='center',va='center')
 pylab.gca().text(3.5,-y0+0.5,'Superheated',ha='center',va='center')
@@ -76,7 +77,7 @@ pylab.gca().text(1.5,-y0-0.1,'$w_{two-phase}$',ha='center',va='center')
 #plot color gradient 2nd plot
 gradient2 = np.linspace(0, 1, 2)
 gradient2 = np.vstack((gradient2, gradient2))
-imgplot = pylab.imshow(gradient2, extent=[-0.01,4.01,-1.7,-0.71], alpha=0.9, aspect=4, cmap=plt.get_cmap('brg'))
+imgplot = pylab.imshow(gradient2, extent=[0.0,4.0,-1.7,-0.71], alpha=0.9, aspect=4, cmap=plt.get_cmap('brg'))
 imgplot.set_clim(-0.2,2.0)
 
 pylab.xlim(-0.1,4.3)
