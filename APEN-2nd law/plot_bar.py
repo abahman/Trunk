1e9a998dce2ffe6a88f4869276058a80fe7001f3
Ammar Bahman <bahman.ammar@gmail.com>
2019-12-05 13:07:58 +0300

    update

--- APEN-2nd law/plot_bar.py
+++ APEN-2nd law/plot_bar.py
@@ -47,11 +47,13 @@ mpl.rcParams['legend.numpoints'] = 1
 # #===============================================================================
 # # END of Latex render
 # #===============================================================================
-
+     
+################################
+##### exergy destruction #######
+################################
 #import data from excel file
 df1 = pd.read_excel('data_tables.xlsx',sheet_name='Exergy_des',header=0) #file name
-df2 = pd.read_excel('data_tables.xlsx',sheet_name='Exergy_ratio',header=0) #file name
-#assign axes
+#exergy destruction
 evap = np.array(df1[:]["Evaporator"])*1000
 suc = np.array(df1[:]["Suction line"])*1000
 comp = np.array(df1[:]["Compressor"])*1000
@@ -60,44 +62,68 @@ cond = np.array(df1[:]["Condenser"])*1000
 liq = np.array(df1[:]["Liquid line"])*1000
 exp = np.array(df1[:]["Expansion valve"])*1000
 
-     
-#########################
-##### exergy destruction #######
-#########################
-width = 0.75       # the width of the bars
-p1 = plt.bar(np.arange(1,4,1),evap,width,color='k',linewidth=0.9,align='center',alpha=0.9,label=r'Evaporator')
-p2 = plt.bar(np.arange(1,4,1),suc,width,bottom=evap,color='b',linewidth=0.9,align='center',alpha=0.9,label=r'Suction line')
-p3 = plt.bar(np.arange(1,4,1),comp,width,bottom=[i+j for i,j in zip(evap, suc)],color='r',linewidth=0.9,align='center',alpha=0.9,label=r'Compressor')
-p4 = plt.bar(np.arange(1,4,1),dis,width,bottom=[i+j+k for i,j,k in zip(evap, suc, comp)],color='g',linewidth=0.9,align='center',alpha=0.9,label=r'Discharge line')
-p5 = plt.bar(np.arange(1,4,1),cond,width,bottom=[i+j+k+l for i,j,k,l in zip(evap, suc, comp, dis)],color='c',linewidth=0.9,align='center',alpha=0.9,label=r'Condenser')
-p6 = plt.bar(np.arange(1,4,1),liq,width,bottom=[i+j+k+l+m for i,j,k,l,m in zip(evap, suc, comp, dis, cond)],color='y',linewidth=0.9,align='center',alpha=0.9,label=r'Liquid line')
-p7 = plt.bar(np.arange(1,4,1),exp,width,bottom=[i+j+k+l+m+n for i,j,k,l,m,n in zip(evap, suc, comp, dis, cond,liq)],color='m',linewidth=0.9,align='center',alpha=0.9,label=r'Expansion valve')
 
-# def autolabel(rects,p,func):
-#     for idx,rect in enumerate(p):
-#         height = rect.get_height()
-#         plt.text(rect.get_x() + rect.get_width()/2., 0.5*height,
-#                 int(func[idx]),
-#                 ha='center', va='center', rotation=0,bbox=dict(color='white'), fontsize=8)
-# autolabel(p1,p1,evap)
-# autolabel(p3,p3,comp)
-def autolabel(rects):
-    """
-    Attach a text label above each bar displaying its height
-    """
-    for rect in rects:
+width = 0.6       # the width of the bars
+p7 = plt.bar(np.arange(1,4,1),exp,width,bottom=evap+suc+comp+dis+cond+liq,color='m',linewidth=0.9,align='center',alpha=0.9,label=r'Expansion valve',hatch=2*'\\')
+p6 = plt.bar(np.arange(1,4,1),liq,width,bottom=evap+suc+comp+dis+cond,color='y',linewidth=0.9,align='center',alpha=0.9,label=r'Liquid line',hatch=1*'//')
+p5 = plt.bar(np.arange(1,4,1),cond,width,bottom=evap+suc+comp+dis,color='c',linewidth=0.9,align='center',alpha=0.9,label=r'Condenser',hatch=2*'x')
+p4 = plt.bar(np.arange(1,4,1),dis,width,bottom=evap+suc+comp,color='g',linewidth=0.9,align='center',alpha=0.9,label=r'Discharge line',hatch=2*'+')
+p3 = plt.bar(np.arange(1,4,1),comp,width,bottom=evap+suc,color='r',linewidth=0.9,align='center',alpha=0.9,label=r'Compressor',hatch=3*'-')
+p2 = plt.bar(np.arange(1,4,1),suc,width,bottom=evap,color='b',linewidth=0.9,align='center',alpha=0.9,label=r'Suction line',hatch=2*'*')
+p1 = plt.bar(np.arange(1,4,1),evap,width,color='orange',linewidth=0.9,align='center',alpha=0.9,label=r'Evaporator',hatch=2*'.')
+ 
+def autolabel(p1,p2,p3,p4,p5,p6,p7,func1,func2,func3,func4,func5,func6,func7):
+    'function used to plot the values on the stacked bar'
+    h = [0,0,0]
+    for idx,rect in enumerate(p1):
         height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-                '%d' % int(height),
-                ha='center', va='bottom')
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()/2., 0.5*h[idx],
+                int(func1[idx]),
+                ha='center', va='center', rotation=0, fontsize=10,color='white')
+    for idx,rect in enumerate(p2):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()+0.08, h[idx]-height/2,
+                int(func2[idx]),
+                ha='center', va='center', rotation=0, fontsize=10,color='k')
+    for idx,rect in enumerate(p3):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()/2., h[idx]-height/2,
+                int(func3[idx]),
+                ha='center', va='center', rotation=0, fontsize=10,color='white')
+    for idx,rect in enumerate(p4):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()+0.08, h[idx]-height/2,
+                int(func4[idx]),
+                ha='center', va='center', rotation=0, fontsize=10,color='k')
+    for idx,rect in enumerate(p5):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()/2., h[idx]-height/2,
+                int(func5[idx]),
+                ha='center', va='center', rotation=0, fontsize=10,color='white')
+    for idx,rect in enumerate(p6):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()+0.08, h[idx]-height/2,
+                round(func6[idx],1),
+                ha='center', va='center', rotation=0, fontsize=10,color='k')
+    for idx,rect in enumerate(p7):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()/2., h[idx]-height/2,
+                int(func7[idx]),
+                ha='center', va='center', rotation=0, fontsize=10,color='white')    
 
-autolabel(p1)
-autolabel(p2)
+#plot the numbers
+autolabel(p1,p2,p3,p4,p5,p6,p7,evap,suc,comp,dis,cond,liq,exp)
 
     
-
-plt.xticks([0, 1, 2, 3, 4],
-           [r'',r'1.5-RT', r'3-RT', r'5-RT', r''])
+plt.xticks([1, 2, 3],
+           [r'1.5-RT', r'3-RT', r'5-RT'])
 plt.tick_params(
     axis='x',          # changes apply to the x-axis
     which='both',      # both major and minor ticks are affected
@@ -106,12 +132,110 @@ plt.tick_params(
     labelbottom=True) # labels along the bottom edge are off
 #plt.xlabel(r'Test condition')
 plt.ylabel(r'$\dot I$ [W]')
-plt.ylim(0,9000)
-leg=plt.legend(loc='upper left',scatterpoints=1,scatteryoffsets=[0.5])
+plt.ylim(0,8000)
+# leg=plt.legend(loc='upper left',scatterpoints=1,scatteryoffsets=[0.5])
+leg = plt.legend(loc='upper center',bbox_to_anchor=(0.5, 1.22),fancybox=False,numpoints=1,ncol=3)#, bbox_to_anchor=(0.5, 1.15)
 frame=leg.get_frame()  
 frame.set_linewidth(0.5)
 plt.tight_layout(pad=0.2)        
 plt.tick_params(direction='in')
-plt.savefig('exergy_des.pdf')
+plt.savefig('irrev2.pdf')
+plt.show()
+plt.close()
+
+
+
+#########################
+##### exergy destruction ratio#######
+#########################
+#import data from excel file
+df2 = pd.read_excel('data_tables.xlsx',sheet_name='Exergy_ratio',header=0) #file name
+#exergy destruction ration
+evap = np.array(df2[:]["Evaporator"])
+suc = np.array(df2[:]["Suction line"])
+comp = np.array(df2[:]["Compressor"])
+dis = np.array(df2[:]["Discharge line"])
+cond = np.array(df2[:]["Condenser"])
+liq = np.array(df2[:]["Liquid line"])
+exp = np.array(df2[:]["Expansion valve"])
+
+
+width = 0.6       # the width of the bars
+p7 = plt.bar(np.arange(1,4,1),exp,width,bottom=evap+suc+comp+dis+cond+liq,color='m',linewidth=0.9,align='center',alpha=0.9,label=r'Expansion valve',hatch=2*'\\')
+p6 = plt.bar(np.arange(1,4,1),liq,width,bottom=evap+suc+comp+dis+cond,color='y',linewidth=0.9,align='center',alpha=0.9,label=r'Liquid line',hatch=1*'//')
+p5 = plt.bar(np.arange(1,4,1),cond,width,bottom=evap+suc+comp+dis,color='c',linewidth=0.9,align='center',alpha=0.9,label=r'Condenser',hatch=2*'x')
+p4 = plt.bar(np.arange(1,4,1),dis,width,bottom=evap+suc+comp,color='g',linewidth=0.9,align='center',alpha=0.9,label=r'Discharge line',hatch=2*'+')
+p3 = plt.bar(np.arange(1,4,1),comp,width,bottom=evap+suc,color='r',linewidth=0.9,align='center',alpha=0.9,label=r'Compressor',hatch=3*'-')
+p2 = plt.bar(np.arange(1,4,1),suc,width,bottom=evap,color='b',linewidth=0.9,align='center',alpha=0.9,label=r'Suction line',hatch=2*'*')
+p1 = plt.bar(np.arange(1,4,1),evap,width,color='orange',linewidth=0.9,align='center',alpha=0.9,label=r'Evaporator',hatch=2*'.')
+ 
+def autolabel_new(p1,p2,p3,p4,p5,p6,p7,func1,func2,func3,func4,func5,func6,func7):
+    'function used to plot the values on the stacked bar'
+    h = [0,0,0]
+    for idx,rect in enumerate(p1):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()/2., 0.5*h[idx],
+                round(func1[idx],1),
+                ha='center', va='center', rotation=0, fontsize=10,color='white')
+    for idx,rect in enumerate(p2):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()+0.08, h[idx]-height/2,
+                round(func2[idx],1),
+                ha='center', va='center', rotation=0, fontsize=10,color='k')
+    for idx,rect in enumerate(p3):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()/2., h[idx]-height/2,
+                round(func3[idx],1),
+                ha='center', va='center', rotation=0, fontsize=10,color='white')
+    for idx,rect in enumerate(p4):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()+0.08, h[idx]-height/2,
+                round(func4[idx],1),
+                ha='center', va='center', rotation=0, fontsize=10,color='k')
+    for idx,rect in enumerate(p5):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()/2., h[idx]-height/2,
+                round(func5[idx],1),
+                ha='center', va='center', rotation=0, fontsize=10,color='white')
+    for idx,rect in enumerate(p6):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()+0.1, h[idx]-height/2,
+                round(func6[idx],2),
+                ha='center', va='center', rotation=0, fontsize=10,color='k')
+    for idx,rect in enumerate(p7):
+        height = rect.get_height()
+        h[idx] += height
+        plt.text(rect.get_x() + rect.get_width()/2., h[idx]-height/2,
+                round(func7[idx],1),
+                ha='center', va='center', rotation=0, fontsize=10,color='white')    
+
+#plot the numbers
+autolabel_new(p1,p2,p3,p4,p5,p6,p7,evap,suc,comp,dis,cond,liq,exp)
+
+    
+plt.xticks([1, 2, 3],
+           [r'1.5-RT', r'3-RT', r'5-RT'])
+plt.tick_params(
+    axis='x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom=False,      # ticks along the bottom edge are off
+    top=False,         # ticks along the top edge are off
+    labelbottom=True) # labels along the bottom edge are off
+#plt.xlabel(r'Test condition')
+plt.ylabel(r'$E_d$ [$\%$]')
+plt.ylim(0,100)
+leg = plt.legend(loc='upper center',bbox_to_anchor=(0.5, 1.22),fancybox=False,numpoints=1,ncol=3)
+# leg=plt.legend(loc='upper left',scatterpoints=1,scatteryoffsets=[0.5])
+frame=leg.get_frame()  
+frame.set_linewidth(0.5)
+plt.tight_layout(pad=0.2)        
+plt.tick_params(direction='in')
+plt.savefig('exergy_ratio2.pdf')
 plt.show()
 plt.close()
